98889bcb0413d9d3a42bf8a1a0955bba06bf1250
Andrey Dorodhykh <deripapaivan@gmail.com>
2025-02-03 18:54:58 +0200

    lololololololo

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,2 @@
-﻿string termo_otrguga = "чокопай 2";
-Console.WriteLine(termo_otrguga);
+﻿int pp = 18;
+Console.WriteLine(pp);
--- ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+843edeacc57b14207bb6a4a0b860519799237f19")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4bb5d885072847ed295e6734d765ae6ef4e17ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
